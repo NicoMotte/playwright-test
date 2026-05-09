ebb85e46be40c7f1555b6ab0390c45fa92c70780
Nicolas Motte <nicolas.l.motte@gmail.com>
2026-05-09 14:33:40 +0200

    Refactorise les tests Dynamic Controls et Add Remove avec états explicites

--- tests/add_remove.spec.ts
+++ tests/add_remove.spec.ts
@@ -1,52 +1,37 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('Delete button appears', async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com/add_remove_elements/');
+//Launches the web page for every test
+test.describe("Testing a button that creates delete buttons", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://the-internet.herokuapp.com/add_remove_elements/");
+  });
 
-  const addButton = page.getByRole('button', { name: 'Add Element' });
-  const deleteButtons = page.getByRole('button', { name: 'Delete' });
-  
-  await expect(deleteButtons).toHaveCount(0);
+  test("Delete button appears", async ({ page }) => {
+    const addButton = page.getByRole("button", { name: "Add Element" });
+    const deleteButtons = page.getByRole("button", { name: "Delete" });
 
-  await addButton.click();
+    await expect(deleteButtons).toHaveCount(0);
 
-  await expect(deleteButtons).toHaveCount(1);
-
-});
-
-
-test('Delete button appears then dissapears', async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com/add_remove_elements/');
-
-  const addButton = page.getByRole('button', { name: 'Add Element' });
-  const deleteButtons = page.getByRole('button', { name: 'Delete' });
-  
-  await expect(deleteButtons).toHaveCount(0);
-
-  for (let i = 0; i < 7; i++) {
     await addButton.click();
-  }
 
-  await expect(deleteButtons).toHaveCount(7);
+    await expect(deleteButtons).toHaveCount(1);
+  });
 
-  await deleteButtons.nth(0).click(); // premier
-  
-  await expect(deleteButtons).toHaveCount(6);
+  test("Delete button appears then disappears", async ({ page }) => {
+    const addButton = page.getByRole("button", { name: "Add Element" });
+    const deleteButtons = page.getByRole("button", { name: "Delete" });
+    const numberOfCreatedButtons = 10;
 
-  await deleteButtons.last().click();
+    await expect(deleteButtons).toHaveCount(0);
 
-  await expect(deleteButtons).toHaveCount(5);
+    for (let i = 0; i < numberOfCreatedButtons; i++) {
+      await addButton.click();
+      await expect(deleteButtons).toHaveCount(i + 1);
+    }
 
-  await deleteButtons.nth(0).click(); // deuxième
-
-  await expect(deleteButtons).toHaveCount(4);
-
-  await deleteButtons.nth(0).click();
-  await deleteButtons.nth(0).click();
-  await deleteButtons.nth(0).click();
-  await deleteButtons.nth(0).click();
-
-  await expect(deleteButtons).toHaveCount(0);
-
-
+    for (let i = 0; i < numberOfCreatedButtons; i++) {
+      await deleteButtons.nth(0).click();
+      await expect(deleteButtons).toHaveCount(numberOfCreatedButtons - 1 - i);
+    }
+  });
 });
--- tests/dynamic_controls.spec.ts
+++ tests/dynamic_controls.spec.ts
@@ -1,53 +1,73 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('Remove/add', async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com/dynamic_controls');
+test.describe("Buttons that create or deactivate page contents", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://the-internet.herokuapp.com/dynamic_controls");
+  });
 
-   
-  const checkBox = page.locator('#checkbox');
-  const removeButton = page.getByRole('button', { name: 'Remove' });
-  const addButton = page.getByRole('button', { name: 'Add' });
+  test("Remove/add", async ({ page }) => {
+    const checkboxExample = page.locator("#checkbox-example");
+    const checkBox = checkboxExample.locator("#checkbox");
+    const removeButton = checkboxExample.getByRole("button", {
+      name: "Remove",
+    });
+    const addButton = checkboxExample.getByRole("button", { name: "Add" });
+    const message = checkboxExample.locator("#message");
 
-  await expect(checkBox).toBeVisible({ timeout: 10000 });
-  await expect(removeButton).toBeVisible({ timeout: 10000 });
-  
-  await removeButton.click();
+    async function expectBoxVisibleState() {
+      await expect(addButton).toBeHidden({ timeout: 10000 });
+      await expect(checkBox).toBeVisible({ timeout: 10000 });
+      await expect(removeButton).toBeVisible({ timeout: 10000 });
+    }
 
-  await expect(checkBox).toBeHidden({ timeout: 10000 });
-  await expect(addButton).toBeVisible({ timeout: 10000 });
+    async function expectBoxRemovedState() {
+      await expect(checkBox).toBeHidden({ timeout: 10000 });
+      await expect(addButton).toBeVisible({ timeout: 10000 });
+      await expect(removeButton).toBeHidden({ timeout: 10000 });
+    }
 
-  await addButton.click();
+    await expectBoxVisibleState();
 
-  await expect(addButton).toBeHidden({ timeout: 10000 });
-  await expect(checkBox).toBeVisible({ timeout: 10000 });
-  await expect(removeButton).toBeVisible({ timeout: 10000 });
+    await removeButton.click();
+
+    await expectBoxRemovedState();
+    await expect(message).toContainText("It's gone!");
+
+    await addButton.click();
+
+    await expectBoxVisibleState();
+    await expect(message).toContainText("It's back!");
+  });
+
+  test("Enable/disable", async ({ page }) => {
+    const inputExample = page.locator("#input-example");
+    const inputField = inputExample.locator("input");
+    const message = inputExample.locator("#message");
+    const disableButton = inputExample.getByRole("button", { name: "Disable" });
+    const enableButton = inputExample.getByRole("button", { name: "Enable" });
+
+    async function expectInputBarDisabledState() {
+      await expect(disableButton).toBeHidden({ timeout: 10000 });
+      await expect(inputField).toBeDisabled({ timeout: 10000 });
+      await expect(enableButton).toBeVisible({ timeout: 10000 });
+    }
+
+    async function expectInputBarEnabledState() {
+      await expect(inputField).toBeEnabled({ timeout: 10000 });
+      await expect(disableButton).toBeVisible({ timeout: 10000 });
+      await expect(enableButton).toBeHidden({ timeout: 10000 });
+    }
+
+    await expectInputBarDisabledState();
+
+    await enableButton.click();
+
+    await expectInputBarEnabledState();
+    await expect(message).toContainText("It's enabled!");
+
+    await disableButton.click();
+
+    await expectInputBarDisabledState();
+    await expect(message).toContainText("It's disabled!");
+  });
 });
-
-
-test('Enable/disable', async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com/dynamic_controls');
-  
-  const inputExample = page.locator('#input-example');
-  const inputField = inputExample.locator('input');
-  const message = inputExample.locator('#message');
-  const disableButton = page.getByRole('button', { name: 'Disable' });
-  const enableButton = page.getByRole('button', { name: 'Enable' });
-  
-  await expect(inputField).toBeDisabled({ timeout: 10000 });
-  await expect(enableButton).toBeVisible({ timeout: 10000 });
-  
-  await enableButton.click();
-
-  await expect(inputField).toBeEnabled({ timeout: 10000 });
-  await expect(disableButton).toBeVisible({ timeout: 10000 });
-  await expect(message).toContainText('It\'s enabled!');
-
-  await disableButton.click();
-
-  await expect(disableButton).toBeHidden({ timeout: 10000 });
-  await expect(inputField).toBeDisabled({ timeout: 10000 });
-  await expect(enableButton).toBeVisible({ timeout: 10000 });
-  await expect(message).toContainText('It\'s disabled!');
-
-});
-
