240ddf96840c3db96c9a2f5b087e40cbced5a125
Nicolas Motte <nicolas.l.motte@gmail.com>
2026-05-09 18:17:56 +0200

    factorisation de 2 tests avec fonction commune pour Dynamic Loading

--- tests/dynamic_loading.spec.ts
+++ tests/dynamic_loading.spec.ts
@@ -1,39 +1,34 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from "@playwright/test";
 
-test('Example 1: Element on page that is hidden', async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com/dynamic_loading');
-  await page.getByRole('link', { name: 'Example 1' }).click();
+async function clickOnStartButton(page: Page) {
+  const startButton = page.getByRole("button", { name: "Start" });
+  const loading = page.locator("#loading");
+  const finish = page.locator("#finish");
 
-  const startButton = page.getByRole('button', { name: 'Start' });
-  
   await expect(startButton).toBeVisible();
-  
   await startButton.click();
 
-  await expect(page.locator('#loading')).toBeVisible();
+  await expect(loading).toBeVisible();
+  await expect(loading).toBeHidden({ timeout: 10000 });
 
-  await expect(page.locator('#loading')).toBeHidden({ timeout: 10000 });
+  await expect(finish).toBeVisible({ timeout: 10000 });
+  await expect(finish).toContainText("Hello World!", { timeout: 10000 });
+}
 
-  await expect(page.locator('#finish')).toBeVisible({ timeout: 10000 });
-  await expect(page.locator('#finish')).toContainText('Hello World!', { timeout: 10000 });
-  
-});
+test.describe("Dynamic loading", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://the-internet.herokuapp.com/dynamic_loading");
+  });
 
-test('Example 2: Element rendered after the fact', async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com/dynamic_loading');
-  await page.getByRole('link', { name: 'Example 2' }).click();
+  test("Example 1: Element on page that is hidden", async ({ page }) => {
+    await page.getByRole("link", { name: "Example 1" }).click();
 
-  const startButton = page.getByRole('button', { name: 'Start' });
-  
-  await expect(startButton).toBeVisible();
-  
-  await startButton.click();
+    await clickOnStartButton(page);
+  });
 
-  await expect(page.locator('#loading')).toBeVisible();
+  test("Example 2: Element rendered after the fact", async ({ page }) => {
+    await page.getByRole("link", { name: "Example 2" }).click();
 
-  await expect(page.locator('#loading')).toBeHidden({ timeout: 10000 });
-
-  await expect(page.locator('#finish')).toBeVisible({ timeout: 10000 });
-  await expect(page.locator('#finish')).toContainText('Hello World!', { timeout: 10000 });
-  
+    await clickOnStartButton(page);
+  });
 });
