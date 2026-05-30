0673e59b9e82a0559b09050eb9f9437e8ff25a53
Nicolas Motte <nicolas.l.motte@gmail.com>
2026-05-30 19:42:19 +0200

    test basique de lecture des requêtes API en plus du résultat visible par l'utilisateur

---
+++ tests/api_basic_example_login.spec.ts
@@ -0,0 +1,68 @@
+import { test, expect } from "@playwright/test";
+
+test("connexion invalide avec observation réseau", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/login");
+
+  const authRequestPromise = page.waitForRequest((request) =>
+    request.url().includes("/authenticate"),
+  );
+
+  const authResponsePromise = page.waitForResponse((response) =>
+    response.url().includes("/authenticate"),
+  );
+
+  await page.getByLabel("Username").fill("tomsmith");
+  await page.getByLabel("Password").fill("mauvaismotdepasse");
+  await page.getByRole("button", { name: "Login" }).click();
+
+  const authRequest = await authRequestPromise;
+  const authResponse = await authResponsePromise;
+
+  console.log("Auth request URL =", authRequest.url());
+  console.log("Auth request method =", authRequest.method());
+
+  expect(authRequest.method()).toBe("POST");
+
+  console.log("Auth URL =", authResponse.url());
+  console.log("Auth status =", authResponse.status());
+
+  expect([302, 303]).toContain(authResponse.status());
+
+  await expect(page.locator("#flash")).toContainText(
+    "Your password is invalid",
+  );
+});
+
+test("connexion valide avec observation réseau", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/login");
+
+  const authRequestPromise = page.waitForRequest((request) =>
+    request.url().includes("/authenticate"),
+  );
+
+  const authResponsePromise = page.waitForResponse((response) =>
+    response.url().includes("/authenticate"),
+  );
+
+  await page.getByLabel("Username").fill("tomsmith");
+  await page.getByLabel("Password").fill("SuperSecretPassword!");
+  await page.getByRole("button", { name: "Login" }).click();
+
+  const authRequest = await authRequestPromise;
+  const authResponse = await authResponsePromise;
+
+  console.log("Auth request URL =", authRequest.url());
+  console.log("Auth request method =", authRequest.method());
+
+  expect(authRequest.method()).toBe("POST");
+
+  console.log("Auth URL =", authResponse.url());
+  console.log("Auth status =", authResponse.status());
+
+  expect(authResponse.status()).toBe(303);
+
+  await expect(page).toHaveURL(/.*secure/);
+  await expect(page.locator("#flash")).toContainText(
+    "You logged into a secure area",
+  );
+});
